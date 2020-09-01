419e94aed2bdf96d2cc81677571de80ef589e23c
xiaochuan <442616909@qq.com>
2020-09-01 10:12:45 +0800

    status

---
+++ src/components/BodyHeader/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <el-page-header @back="goBack" content="详情页面">
+    </el-page-header>
+    ddffdfff
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      goBack() {
+        console.log('go back');
+      }
+    }
+  }
+</script>
